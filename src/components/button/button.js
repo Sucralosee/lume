8a4f74bfe4f9cb6b936e273a2c66892068136399
Ben Louis <142682578+Sucralosee@users.noreply.github.com>
2025-03-25 14:32:43 -0700

    better button animations

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -7,39 +7,46 @@ import './button.css';
 
 const Button = ({ onClick, children }) => {
 
-    useEffect(() => {
-        const buttons = document.querySelectorAll('.btn');
-        
-        buttons.forEach(button => {
-          const hoverTimeline = gsap.timeline({ paused: true });
-          
-          hoverTimeline
-            .to(button.querySelector('::before') || button.childNodes[0], {
-              scaleX: 1,
-              duration: 0.1,
-              ease: "power2.out"
-            }, 0)
-            .to(button, {
-              color: "white",
-              duration: 0.05,
-              ease: "power1.out"
-            }, 0.1);
-          
-          button.addEventListener('mouseenter', () => hoverTimeline.play());
-          button.addEventListener('mouseleave', () => hoverTimeline.reverse());
-        });
-        
-        return () => {
-          buttons.forEach(button => {
-            button.removeEventListener('mouseenter', () => {});
-            button.removeEventListener('mouseleave', () => {});
-          });
-        };
-      }, []);
+  useEffect(() => {
+    const buttons = document.querySelectorAll('.btn');
+    
+    buttons.forEach(button => {
+      const hoverTimeline = gsap.timeline({ paused: true });
+      
+      hoverTimeline
+        .to(button.querySelector('::before') || button, {
+          scaleX: 1,
+          duration: 0.3,
+          ease: "power2.out"
+        }, 0)
+        .to(button, {
+          color: "white",
+          duration: 0.3,
+          ease: "power1.out"
+        }, 0);
+      
+      const mouseEnterHandler = () => {
+        gsap.killTweensOf(button, "color");
+        hoverTimeline.play();
+      };
+      
+      const mouseLeaveHandler = () => {
+        hoverTimeline.reverse();
+      };
+      
+      button.addEventListener('mouseenter', mouseEnterHandler);
+      button.addEventListener('mouseleave', mouseLeaveHandler);
+      
+      return () => {
+        button.removeEventListener('mouseenter', mouseEnterHandler);
+        button.removeEventListener('mouseleave', mouseLeaveHandler);
+      };
+    });
+  }, []);
 
 
   return (
-    <button onClick={onClick} className='btn border-red-500' >
+    <button onClick={onClick} className='btn' >
       {children}
     </button>
   );
