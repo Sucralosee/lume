8da14a28e8d24b49741ddd07ab52d59389669706
Sucralosee <benlouis360@gmail.com>
2025-10-06 14:42:19 -0700

    clean up ui

--- src/components/instructionsComp/Instructions.js
+++ src/components/instructionsComp/Instructions.js
@@ -5,7 +5,7 @@ const Instructions = ({num , instruct}) => {
 
     return (
         <section className="instructions-container">
-            <p className="num">{num}.</p>
+            {/* <p className="num">{num}.</p> */}
             <p className="instruct">{instruct}</p>
         </section>
     );
